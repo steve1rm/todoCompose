5a342e3be44b86f2a05f9177b87085a2ddb5df70
Steve <steve@kitkat@gmail.com>
2022-05-04 23:00:36 +0700

    Add the getting all tasks in the shareviewmodel

--- app/src/main/java/me/androidbox/todocompose/viewmodel/ShareViewModel.kt
+++ app/src/main/java/me/androidbox/todocompose/viewmodel/ShareViewModel.kt
@@ -1,10 +1,29 @@
 package me.androidbox.todocompose.viewmodel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import me.androidbox.domain.entity.TodoTaskEntity
+import me.androidbox.domain.usecase.GetAllTaskUseCase
 import javax.inject.Inject
 
+@HiltViewModel
 class ShareViewModel @Inject constructor(
-
+    private val getAllTaskUseCase: GetAllTaskUseCase
 ): ViewModel() {
+
+    private val listOfTaskMutableStateFlow = MutableStateFlow<List<TodoTaskEntity>>(emptyList())
+    val listOfTaskStateFlow = listOfTaskMutableStateFlow.asStateFlow()
+    
+    fun getAllTasks() {
+        viewModelScope.launch {
+            getAllTaskUseCase.getAllTask().collect { listOfTodoTask ->
+                listOfTaskMutableStateFlow.value = listOfTodoTask
+            }
+        }
+    }
 
 }
